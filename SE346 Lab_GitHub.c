bfbd1f7fac18f417c50d1f339f5e9d1ab382e27b
batuhan-karatas <karatas.batuhan@outlook.com>
2022-05-13 16:58:30 +0300

    v1.2

--- SE346 Lab_GitHub.c
+++ SE346 Lab_GitHub.c
@@ -6,6 +6,8 @@ int main()
 	
 	//please write your name,student id and department by using printf() function
 
+	printf("Name: Fazlı Batuhan Karataş \nStudent id: 18243610015 \nDepartment: Information System Engineering\n");
+
 	printf("Z.Yaren Oguz,123456,SE\n");
 
 	printf("Ozge Tekin,987654,SE\n");
